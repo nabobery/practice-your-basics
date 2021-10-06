5b000730f051b4589bd8b4e35c0cd3d318ff754b
Himanshu Malviya <79209336+malviyaHimanshu@users.noreply.github.com>
2021-10-06 19:08:28 +0530

    Solved Question 16 using Python

---
+++ python/Quesn16.py
@@ -0,0 +1,13 @@
+# Name : Himanshu
+# Roll No. : CS20B1097
+
+'''
+Question 16.
+Define a function that can accept two strings as input and 
+concatenate them and then print it in console.
+'''
+
+string1 = input("Enter First String : ")
+string2 = input("Enter Second String : ")
+
+print(string1, string2)
